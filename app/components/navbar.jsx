07e2953887d6e4a504909ec8f8ad9ff4bd0506d8
Ed Monteiro <edemirmonteir@gmail.com>
2017-06-17 12:28:04 +0100

    Add initial components

---
+++ app/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export class navbar extends React.Component {
+    render(){
+      return (
+        <p>test</p>
+        // <div class="alert alert-success" role="alert">
+        //     <strong>Well done!</strong> You successfully read this important alert message.
+        // </div>
+      );
+    }
+}
