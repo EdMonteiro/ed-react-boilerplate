278c24de4d31ce103394806c582ec97dabda1bf2
Ed Monteiro <edemirmonteir@gmail.com>
2017-06-17 11:25:55 +0100

    Initial nav bar

---
+++ app/components/nav-bar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export class nav-bar extends React.Component {
+    render(){
+      return (
+        <Navbar className="navbar-dark" fluid>
+        ...
+        </Navbar>
+      );
+    }
+}
