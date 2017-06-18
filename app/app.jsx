53d5b34c13ee17f162272de70709d024e8b70e47
Ed Monteiro <edemirmonteir@gmail.com>
2017-06-18 13:44:23 +0100

    Create main and header

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,13 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-import navbar from "./components/navbar.jsx";
+import navbar from "./components/main";
 
 
 // App css
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <navbar/>,
+  <main/>,
   document.getElementById('app')
 );
